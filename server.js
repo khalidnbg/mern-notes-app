4d11646a637f83ba9236902a93888aff95771df9
khalidnbg <khalidnabgaoui4@gmail.com>
2024-03-13 15:17:00 +0000

    add morgan logger

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import morgan from "morgan";
 
 dotenv.config({
   path: "./config/config.env",
 });
 
 const app = express();
+app.use(morgan("dev"));
 
 app.use(express.json());
 
